370827b585416ccaaefd199668843822870d099d
Joseph Schell <JosephSchell510@gmail.com>
2018-07-31 23:39:18 -0700

    Started creating neighbor map.

---
+++ Assets/Scripts/Cell.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cell : MonoBehaviour
+{
+	[SerializeField] private List<Cell> neighbors = new List<Cell>();
+	public bool Visited = false;
+
+	public void TargetNeighbors()
+	{
+		foreach (Cell neighbor in neighbors)
+		{
+			Debug.DrawLine(transform.position, neighbor.transform.position/*+ new Vector3(0, 1, 0)*/, Color.blue, 150);
+		}
+
+
+		//Debug.Log("Here.");
+		//Debug.DrawLine(transform.position, transform.position + new Vector3(0, 1, 0), Color.blue, 5);
+	}
+
+	public Cell GetRandomNeighbor()
+	{
+		int random = Random.Range(0, neighbors.Count);
+
+		if(neighbors[random].Visited == true)
+		{
+			return null;
+		}
+		else
+		{
+			return neighbors[random];
+		}
+	}
+}
---
+++ Assets/Scripts/Maze.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Maze : MonoBehaviour
+{ 
+	[SerializeField] private List<Cell> cells = new List<Cell>();
+
+	private void Start()
+	{
+		//foreach (Cell cell in cells)
+		//{
+		//	cell.TargetNeighbors();
+		//}
+
+		BuildEdge(cells[5]);
+	}
+
+	// Generate Maze
+
+	private void BuildEdge(Cell cell)
+	{
+		cell.Visited = true;
+		Cell neighbor = cell.GetRandomNeighbor();
+
+		if(neighbor != null)
+		{
+			Debug.Log("Null Neighbor!?" + neighbor.name);
+			Debug.DrawLine(cell.transform.position, neighbor.transform.position, Color.green, 10);
+
+			BuildEdge(neighbor);
+		}
+	}
+}
